66da3f3b16229fe7c373195185377793d8592a51
Ana Eliza <anaelizaperobelli@gmail.com>
2023-08-05 23:36:39 -0300

    User entity and resource

---
+++ src/main/java/com/devesuperior/course/resource/UserResource.java
@@ -0,0 +1,20 @@
+package com.devesuperior.course.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devesuperior.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Maria", "mariagmail", "294774", "122345");
+		return ResponseEntity.notFound().build();
+	}
+
+}
